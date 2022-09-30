831a521c166dc460755fd170025975df01b28c1a
Engel62 <sr.engel@bk.ru>
2022-09-30 13:41:31 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     var hoursWork = 640;
     var personHours = 8;
     var allWorkers = hoursWork / personHours;
-    System.out.println("Всего рабоников компании - " + allWorkers + " человек");
+    System.out.println("Всего рабоников компании - " + allWorkers + " человек.");
+    allWorkers = allWorkers + 94;
+    var allHours = hoursWork / allWorkers;
+    System.out.println("Если в компании работает " + allWorkers + " человек, то всего " + allHours + " часа работы может быть поделено между сотрудниками.");
     }
 }
